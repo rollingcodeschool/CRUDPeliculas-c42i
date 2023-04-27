07b22ab8e9b7e23a9e48baaf46f1c8633f7645a9
Emilse Arias <emilsearias@gmail.com>
2023-04-27 17:28:31 -0300

    creo la pelicula con los datos del formulario

--- js/administrador.js
+++ js/administrador.js
@@ -37,22 +37,27 @@ function prepararFormularioPelicula(e) {
 
 function crearPelicula() {
   //validar los datos
-  const resumen = resumenValidaciones(titulo.value, descripcion.value, imagen.value, genero.value);
+  const resumen = resumenValidaciones(
+    titulo.value,
+    descripcion.value,
+    imagen.value,
+    genero.value
+  );
   //si los datos son validos?
   //esta funcion decide si muestra o no el mensaj de error
-  mostrarMensajeError(resumen)
+  mostrarMensajeError(resumen);
   if (resumen.length === 0) {
     // crear pelicula
     const peliculaEjemplo = new Pelicula(
-      "0001",
-      "El Padrino",
-      "La familia Corleone es una de las más poderosas de Nueva York en los años 40.",
-      "https://imagenes.psicologiaymente.com/wp-content/uploads/2021/02/el-padrino-1.png",
-      "Drama/Crimen",
-      1972,
-      "2h 55min",
-      "Estados Unidos",
-      ["Marlon Brando", "Al Pacino", "James Caan"]
+      undefined,
+      titulo.value,
+      descripcion.value,
+      imagen.value,
+      genero.value,
+      anio.value,
+      duracion.value,
+      pais.value,
+      reparto.value
     );
     //agregar la Pelicula en el arreglo de peliculas
     //guardar el array en localstorage
@@ -60,11 +65,11 @@ function crearPelicula() {
   }
 }
 
-function mostrarMensajeError(resumen){
-  if(resumen.length > 0){
-    alert.className = 'alert alert-danger mt-3';
-    alert.innerHTML = resumen
-  }else{
-    alert.className = 'alert alert-danger mt-3 d-none';
+function mostrarMensajeError(resumen) {
+  if (resumen.length > 0) {
+    alert.className = "alert alert-danger mt-3";
+    alert.innerHTML = resumen;
+  } else {
+    alert.className = "alert alert-danger mt-3 d-none";
   }
 }
