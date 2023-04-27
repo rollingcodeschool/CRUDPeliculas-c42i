90f7bf16d3c0cf463b35fc6956d75052ee2113f5
Emilse Arias <emilsearias@gmail.com>
2023-04-27 18:43:17 -0300

    dibujamos las pelis del localstorage en la tabla

--- js/administrador.js
+++ js/administrador.js
@@ -7,20 +7,56 @@ let modalPelicula = new bootstrap.Modal(
   document.getElementById("modalPelicula")
 );
 const btnCrearPelicula = document.querySelector("#btnCrearPelicula");
-let listaPeliculas = [];
 let codigo = document.getElementById("codigo"),
-  titulo = document.getElementById("titulo"),
-  descripcion = document.getElementById("descripcion"),
-  imagen = document.getElementById("imagen"),
-  genero = document.getElementById("genero"),
-  pais = document.getElementById("pais"),
-  director = document.getElementById("director"),
-  reparto = document.getElementById("reparto"),
-  anio = document.getElementById("anio"),
-  duracion = document.getElementById("duracion"),
-  alert = document.getElementById("alerta");
+titulo = document.getElementById("titulo"),
+descripcion = document.getElementById("descripcion"),
+imagen = document.getElementById("imagen"),
+genero = document.getElementById("genero"),
+pais = document.getElementById("pais"),
+director = document.getElementById("director"),
+reparto = document.getElementById("reparto"),
+anio = document.getElementById("anio"),
+duracion = document.getElementById("duracion"),
+alert = document.getElementById("alerta");
 
-  
+let listaPeliculas =  JSON.parse(localStorage.getItem('listaPeliculas')) || [];
+
+//si tengo peliculas almacenadas en el array las transformo en tipo Pelicula
+if(listaPeliculas.length > 0){
+  listaPeliculas = listaPeliculas.map((pelicula)=> new Pelicula(pelicula.codigo, pelicula.titulo, pelicula.descripcion, pelicula.imagen, pelicula.genero, pelicula.anio, pelicula.duracion, pelicula.pais, pelicula.reparto) )
+}
+console.log(listaPeliculas)
+
+// {...pelicula}
+// pelicula.codigo, pelicula.titulo, pelicula.
+cargaInicial();
+
+function cargaInicial(){
+  if(listaPeliculas.length > 0){
+    listaPeliculas.map((pelicula)=> crearFila(pelicula) )
+  }
+}
+
+function crearFila(pelicula){
+  console.log(pelicula)
+  let tablaPelicula = document.getElementById('tablaPelicula');
+  tablaPelicula.innerHTML += `<tr>
+  <th scope="row">1</th>
+  <td>${pelicula.titulo}</td>
+  <td><span class="my-class text-truncate">${pelicula.descripcion}</span></td>
+  <td><span class="my-class text-truncate">${pelicula.imagen}</span></td>
+  <td>${pelicula.genero}</td>
+  <td>
+    <button class="btn btn-warning" onclick="editarPelicula('${pelicula.codigo}')">
+      <i class="bi bi-pencil-square"></i>
+    </button>
+    <button class="btn btn-danger" onclick="borrarPelicula('${pelicula.codigo}')">
+      <i class="bi bi-x-square"></i>
+    </button>
+  </td>
+</tr>`
+}
+
 //manejadores de eventos
 formularioPeliculas.addEventListener("submit", prepararFormularioPelicula);
 btnCrearPelicula.addEventListener("click", desplegarModalPelicula);
